23a94b234fbb7681df6cb9fe54891493c4ba0db3
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2015-11-14 06:40:44 -0400

    limpieza del mapa y correcion del api key

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,34 +1,72 @@
-angular.module('starter.directives', [])
+angular.module('starter.directives',[])
+.directive('map',function(){
+    return {
+        restrict:'E'
+      , scope:{
+            onCreate:'&'
+        }
+      , link:function($scope,$element,$attr){
+            function initialize(){
+                var styles=[{
+                        stylers:[{
+                            hue:'#00ffe6'
+                        },{
+                            saturation:-20
+                        }]
+                    },{
+                        featureType:'road',
+                        elementType:'geometry',
+                        stylers:[{
+                            lightness:100
+                        },{
+                            visibility:'simplified'
+                        }]
+                    },{
+                        featureType:'road',
+                        elementType:'labels',
+                        stylers:[{
+                            visibility:'on'
+                        }]
+                    },{
+                        featureType:"poi.business",
+                        elementType: "labels",
+                        stylers: [{
+                            visibility: "off"
+                        }]
+                    }]
+                  , styledMap=new google.maps.StyledMapType(styles,{name:'Styled Map'})
+                  , mapOptions={
+                        zoom: 18,
+                        center:new google.maps.LatLng(-17.398606,-66.1527474),
+                        disableDefaultUI:true,
+                        zoomControl:true,
+                        scaleControl:true,
+                        mapTypeControlOptions:{
+                            mapTypeIds:[google.maps.MapTypeId.ROADMAP,'map_style']
+                        }
+                    }
+                  , map=new google.maps.Map($element[0],mapOptions)
 
-.directive('map', function() {
-  return {
-    restrict: 'E',
-    scope: {
-      onCreate: '&'
-    },
-    link: function ($scope, $element, $attr) {
-      function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(-17.398606,-66.1527474),
-          zoom: 18,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map($element[0], mapOptions);
-  
-        $scope.onCreate({map: map});
+                map.mapTypes.set('map_style',styledMap);
+                map.setMapTypeId('map_style');
 
-        // Stop the side bar from dragging when mousedown/tapdown on the map
-        google.maps.event.addDomListener($element[0], 'mousedown', function (e) {
-          e.preventDefault();
-          return false;
-        });
-      }
+                $scope.onCreate({map:map});
 
-      if (document.readyState === "complete") {
-        initialize();
-      } else {
-        google.maps.event.addDomListener(window, 'load', initialize);
-      }
+                // Stop the side bar from dragging when mousedown/tapdown
+                // on the map
+                google.maps.event.addDomListener($element[0],'mousedown',
+                function(e){
+                    e.preventDefault();
+                    return false;
+                });
+            }
+
+            if(document.readyState==='complete'){
+                initialize();
+            }else{
+                google.maps.event.addDomListener(window,'load',initialize);
+            }
+        }
     }
-  }
 });
+
